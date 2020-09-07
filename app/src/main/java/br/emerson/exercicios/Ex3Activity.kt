ee668688ea6ff4024f0cf43aaefbe1d029ab007d
Emerson Costa <syphonemerson@yahoo.com.br>
2020-09-07 13:59:47 -0300

    Exercicio 3 finalizado

--- app/src/main/java/br/emerson/exercicios/Ex3Activity.kt
+++ app/src/main/java/br/emerson/exercicios/Ex3Activity.kt
@@ -2,10 +2,47 @@ package br.emerson.exercicios
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_ex3.*
 
 class Ex3Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ex3)
+
+        btnCalcular.setOnClickListener {
+            var resultadoMSG = ""
+            var resultado = 0
+
+            if ( etValor1.text.isEmpty() || etValor2.text.isEmpty() )
+            {
+                resultadoMSG = "Valores não foram preenchidos"
+            }
+
+            if ( rbSomar.isChecked )
+            {
+                resultado = etValor1.text.toString().toInt() + etValor2.text.toString().toInt()
+                resultadoMSG = "${etValor1.text} + ${etValor2.text} = $resultado"
+            }
+
+            if ( rbSubtrair.isChecked)
+            {
+                resultado = etValor1.text.toString().toInt() - etValor2.text.toString().toInt()
+                resultadoMSG = "${etValor1.text} - ${etValor2.text} = $resultado"
+            }
+
+            if ( rbMultiplicar.isChecked )
+            {
+                resultado = etValor1.text.toString().toInt() * etValor2.text.toString().toInt()
+                resultadoMSG = "${etValor1.text} * ${etValor2.text} = $resultado"
+            }
+
+            if ( rbDividir.isChecked )
+            {
+                resultado = etValor1.text.toString().toInt() / etValor2.text.toString().toInt()
+                resultadoMSG = "${etValor1.text} / ${etValor2.text} = $resultado"
+            }
+
+            alert("Resultado","$resultadoMSG",this)
+        }
     }
 }
