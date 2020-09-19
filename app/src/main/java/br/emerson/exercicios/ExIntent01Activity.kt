c6edbba7a4dda80bf6cc6b282eda5c31e3e90d49
Emerson Costa <syphonemerson@yahoo.com.br>
2020-09-19 15:19:10 -0300

    Exercicio Intent 01 - Finalizado

--- app/src/main/java/br/emerson/exercicios/ExIntent01Activity.kt
+++ app/src/main/java/br/emerson/exercicios/ExIntent01Activity.kt
@@ -1,11 +1,18 @@
 package br.emerson.exercicios
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_ex_intent01.*
 
 class ExIntent01Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ex_intent01)
+
+        btnExIntent01.setOnClickListener {
+            val intentDetalhe = Intent( this, ExIntent01DetalheActivity::class.java )
+            startActivity( intentDetalhe )
+        }
     }
 }
--- app/src/main/java/br/emerson/exercicios/ExIntent01DetalheActivity.kt
+++ app/src/main/java/br/emerson/exercicios/ExIntent01DetalheActivity.kt
@@ -2,10 +2,15 @@ package br.emerson.exercicios
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_ex_intent01_detalhe.*
 
 class ExIntent01DetalheActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ex_intent01_detalhe)
+
+        btnVoltarExIntentDetalhe01.setOnClickListener {
+            finish()
+        }
     }
 }
